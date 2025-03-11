a95aae684d28e7192cd92f9202eab2791b64bdef
Gabriel <ferabela6@gmail.com>
2025-03-11 20:51:36 -0300

    Fixed negative numbers existing

--- src/script.js
+++ src/script.js
@@ -1,11 +1,20 @@
 let count = document.getElementById("count");
 
 document.getElementById("increment").addEventListener("click", function() {
-    count.innerText + 1; // BUG 3: Não está atribuindo o novo valor
+    var numero = count.innerHTML;
+    numero++;
+    count.innerHTML = numero; // BUG 3: Não está atribuindo o novo valor
 });
 
 document.getElementById("decrement").onclick = function() {
-    count.innerText = count.innerText - 1; // BUG 4: Trata innerText como string
+    var numero = count.innerHTML;
+    if(numero > 0){
+        numero--;
+        count.innerHTML = numero;
+    }else{
+        count.innerHTML = numero;
+    }
+    
 };
 
 // BUG 5: Permite valores negativos
