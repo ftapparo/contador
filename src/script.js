2a6f7e80aee003afd636ade15ffe41800e0c1c4f
Beatriz Almeida <Beatrizalmeida310804@gmail.com>
2025-03-11 21:10:20 -0300

    bugfix4
     correção trata innerText como string

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ document.getElementById("increment").addEventListener("click", function() {
 });
 
 document.getElementById("decrement").onclick = function() {
-    count.innerText = count.innerText - 1; // BUG 4: Trata innerText como string
+    count.innerText --; // BUG 4: Trata innerText como string
 };
 
 // BUG 5: Permite valores negativos
