2839e41b5e42ecc7ddd55aa30f81c0b386b01b14
vitor-tardivo <vitorgt.ovidart@hotmail.com>
2025-03-11 21:44:42 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,14 +1,13 @@
 let count = document.getElementById("count");
 
 document.getElementById("increment").addEventListener("click", function() {
-    count.innerText ++; // BUG 3: Não está atribuindo o novo valor (bugfix3-corrigido)
+    count.innerText = parseInt(count.innerText) + 1; // BUG 3: Não está atribuindo o novo valor (bugfix3-corrigido)
 });
 
 document.getElementById("decrement").onclick = function() {
-    //count.innerText = count.innerText - 1; // BUG 4: Trata innerText como string
-
-    if (count.innerText != 0) {// BUG 5: Permite valores negativos (bugfix5-corrigido)
-        count.innerText --; 
+    // BUG 4: Trata innerText como string (bugfix4-corrigido)
+    // BUG 5: Permite valores negativos (bugfix5-corrigido)
+    if (count.innerText > 0) {
+        count.innerText = parseInt(count.innerText) - 1; 
     }
 };
-
