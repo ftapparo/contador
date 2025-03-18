0a41e9b4e2c176dd4b7969144ca41bf660ed4d44
unknown <vini3821>
2025-03-17 23:36:02 -0300

    Corrige bug 3: Ajusta incremento do contador

--- src/script.js
+++ src/script.js
@@ -1,14 +1,13 @@
 let count = document.getElementById("count");
 
 document.getElementById("increment").addEventListener("click", function() {
-    count.innerText + 1; // BUG 3: Não está atribuindo o novo valor
+    count.innerText + 1; 
 });
 
 document.getElementById("decrement").onclick = function() {
     let currentValue = parseInt(count.innerText);
-    if (currentValue > 0) { // Impede valores negativos
+    if (currentValue > 0) { 
         count.innerText = currentValue - 1;
     }
 };
 
-// BUG 5: Permite valores negativos
