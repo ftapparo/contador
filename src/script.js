bc58182f3924fe1561da14fa347bd1c72bd98b36
CaiohSantana <caiodesantana2608@outlook.com>
2025-03-18 13:27:20 -0300

    Correção do BUG 4

--- src/script.js
+++ src/script.js
@@ -1,11 +1,9 @@
 let count = document.getElementById("count");
 
-document.getElementById("increment").addEventListener("click", function() {
-    count.innerText + 1; // BUG 3: Não está atribuindo o novo valor
+document.getElementById("increment").addEventListener("click", function () {
+    count.innerText = parseInt(count.innerText) + 1;
 });
 
 document.getElementById("decrement").onclick = function() {
-    count.innerText = count.innerText - 1; // BUG 4: Trata innerText como string
+    count.innerText = parseInt(count.innerText) - 1;
 };
-
-// BUG 5: Permite valores negativos
