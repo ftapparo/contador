aa302279e040ff803791f21e7268640c8e0066e1
RUANPC\ruang <ruangustavo.ig@hotmail.com>
2025-03-26 20:56:42 -0300

    Erro 4: Usado parsent parsent para forçar o innertext a ser um numero

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 let count = document.getElementById("count");
 
 document.getElementById("increment").onclick = function() {
-    count.innerText = count.innerText + 1; // BUG 4: Trata innerText como string
+    count.innerText = parseInt(count.innerText) + 1; // BUG 4: Trata innerText como string
 };
 
 document.getElementById("decrement").onclick = function() {
-    count.innerText = count.innerText - 1; // BUG 4: Trata innerText como string
+    count.innerText = parseInt(count.innerText) - 1; // BUG 4: Trata innerText como string
 };
 
 // BUG 5: Permite valores negativos
