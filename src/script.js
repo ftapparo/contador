f02f27e8809f16845e0ec1f81dd1d33ea83a634d
DESKTOP-VNBD8DN\User <graminhaanarita@gmail.com>
2025-03-26 21:13:39 -0300

    Correção do erro3, na atribuição do valor.

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 let count = document.getElementById("count");
 
 document.getElementById("increment").addEventListener("click", function() {
-    count.innerText + 1; // BUG 3: Não está atribuindo o novo valor
-});
+    count.innerText = parseInt(count.innerText) + 1;
+}); // BUG 3: está atribuindo o novo valor após a alteração.
 
 document.getElementById("decrement").onclick = function() {
     count.innerText = count.innerText - 1; // BUG 4: Trata innerText como string
