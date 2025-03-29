dda1f4802f95ff8f102d4d54beb9135e0c992ac9
Thiago <thiagolongo012@gmail.com>
2025-03-29 14:54:01 -0300

    Correção na adição de valores do contado

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 let count = document.getElementById("count");
 
 document.getElementById("increment").addEventListener("click", function() {
-    count.innerText + 1; // BUG 3: Não está atribuindo o novo valor
+    count.innerText = Number(count.innerText) + 1;
 });
 
 document.getElementById("decrement").onclick = function() {
