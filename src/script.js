57ee12ca17d6c4c2543838fdfd73365c4fe4940c
JHAndrade27 <jhandrade2715@gmail.com>
2025-03-30 14:36:57 -0300

    bug 3 adicionando parseInt(count.innerText) para converter a string em int >> bug 4 aqui troquei count.innerText para parseInt(count.innerText) tambem para converter string para int funcionando o botão para subtrair um numero a menos >> bug 5 Adiciona verificação de valor mínimo no decremento do contador. >> Impede que o contador exiba valores negativos. >> Garante que o decremento só ocorra se o valor atual for maior que zero.

--- src/script.js
+++ src/script.js
@@ -4,7 +4,12 @@ document.getElementById("increment").addEventListener("click", function() {
     count.innerText = parseInt(count.innerText) + 1; // BUG 3: Não está atribuindo o novo valor
 });
 
-document.getElementById("decrement") .onclick = function() {
-    count.innerText =  parseInt(count.innerText) - 1; // BUG 4:Trata innerText como string
-}
+
+document.getElementById("decrement").onclick = function() {
+    if (parseInt(count.innerText) > 0) {
+        count.innerText = parseInt(count.innerText) - 1;
+    }
+};
+
+
 // BUG 5: Permite valores negativos
