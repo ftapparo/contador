3d2dfeab59b1b26837d8f7296240f7aa15b9b1e2
DESKTOP-VNBD8DN\User <anaantonio087@gmail.com>
2025-03-31 21:32:50 -0300

    Correção do erro 04, transformando em uma string

--- src/script.js
+++ src/script.js
@@ -1,12 +1,11 @@
 let count = document.getElementById("count");
 
 document.getElementById("increment").addEventListener("click", function() {
- 
-    count.innerText = Number(count.innerText) + 1; // BUG 3: Não está atribuindo o novo valor
+    count.innerText = Number(count.innerText) + 1;
 });
 
 document.getElementById("decrement").onclick = function() {
-    count.innerText = count.innerText - 1; // BUG 4: Trata innerText como string
+    count.innerText = Number(count.innerText) - 1; // BUG 4: Trata innerText como string
 };
 
 // BUG 5: Permite valores negativos
