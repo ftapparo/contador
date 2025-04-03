06fe14cd80602a6c43e8e389618f16abc18ded80
DESKTOP-VNBD8DN\User <graminhaanarita@gmail.com>
2025-04-02 22:12:33 -0300

    Correção do erro 4

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ document.getElementById("increment").addEventListener("click", function() {
 }); // BUG 3: está atribuindo o novo valor após a alteração.
 
 document.getElementById("decrement").onclick = function() {
-    count.innerText = count.innerText - 1; // BUG 4: Trata innerText como string
+    count.innerText = parseInt(count.innerText) - 1; // BUG 4: Trata innerText como string
 };
 
 // BUG 5: Permite valores negativos
